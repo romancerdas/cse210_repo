027910182a0ab41a2f2b76268bd0808ec2ca5fb7
romancerdas <159666844+romancerdas@users.noreply.github.com>
2025-06-19 18:32:36 -0600

    Finished learning activity

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -5,5 +5,20 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning05 World!");
+
+
+        List<Shape> myList = new List<Shape>();
+        Square mySquare = new Square("red", 4);
+        Rectangle myRectangle = new Rectangle("blue", 3, 4);
+        Circle myCircle = new Circle("green", 3);
+        myList.Add(mySquare);
+        myList.Add(myRectangle);
+        myList.Add(myCircle);
+
+        foreach (Shape shape in myList) {
+            Console.WriteLine(shape.GetColor());
+            Console.WriteLine(shape.GetArea());
+        }
+
     }
 }
